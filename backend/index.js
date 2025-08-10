6e2c356efc81dbc8ff00da6db26a3cf11ac8bb24
SHEKHAR SHARMA <112850260+TESHEKHAR@users.noreply.github.com>
2025-08-10 17:27:42 +0530

    console hello world

--- backend/index.js
+++ backend/index.js
@@ -41,4 +41,10 @@ app.use('/api',orderRoutes);
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    console.log('Hello World');
+});
+
